c30ad7d17feb6641cc76991afda40afe3b06801b
DPrak <66351514+DPrak@users.noreply.github.com>
2020-06-08 20:10:21 +0530

    Add files via upload

---
+++ Firstproject/src/assignment/Greater.java
@@ -0,0 +1,22 @@
+package assignment;
+import java.util.Scanner;
+
+
+public class Greater {
+	private int num1,num2,num3;
+	Scanner scanner;
+	
+	Greater() {
+		scanner = new Scanner(System.in);
+	
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
